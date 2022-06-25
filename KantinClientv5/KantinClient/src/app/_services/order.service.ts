3616094eb6371dd4b8ecd0b3b9dd1364334db31a
abdullahberbeer <104263146+abdullahberbeer@users.noreply.github.com>
2022-06-26 00:56:15 +0300

    KantinClientv5
    OrderItem işlemleri tamamlandı

---
+++ KantinClientv5/KantinClient/src/app/_services/order.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderListDto } from '../_dto/orderDto/orderListDto.model';
+import { Order } from '../_models/order.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  baseUrl:string='https://localhost:5001';
+  constructor(private http:HttpClient) { }
+  getOrders():Observable<OrderListDto>{
+    return this.http.get<OrderListDto>(this.baseUrl+'/Order');
+  }
+  addPayment(orderId:number):Observable<any>{
+    return this.http.put(this.baseUrl+'/Order/orderDelete/'+orderId,orderId);
+  }
+
+}
